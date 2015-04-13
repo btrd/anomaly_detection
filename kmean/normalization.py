39d00baf620a54d7a0b4cfe05e52ec90a192c743
Jean Bertrand <jeanus24@gmail.com>
2015-04-13 11:49:37 +0200

    Change norma to numpy

--- kmean/normalization.py
+++ kmean/normalization.py
@@ -1,39 +1,32 @@
 import csv
+import numpy as np
 
 class Normalizer():
-    def conv(self, i, j, s):
-        try:
-            s = float(s)
-        except ValueError:
-            if j in self.colFloat:
-                if (self.header and i > 0) or not self.header:
-                    self.colFloat.remove(j)
-        return s
-
     def __init__(self, dataFile, header):
-        self.data = []
-        defCol = True
+        self.dataFile = dataFile
         self.header = header
-        with open(dataFile, 'rU') as desc:
-            reader = csv.reader(desc)
-            i = 0
-            for row in reader:
-                if defCol:
-                    defCol = False
-                    self.colFloat = [x for x in range(len(row))]
-                rowRes = []
-                j = 0
-                for cell in row:
-                    cell = self.conv(i, j, cell)
-                    rowRes.append(cell)
-                    j += 1
-                self.data.append(rowRes)
-                i += 1
-        if header:
-            self.data.pop(0)
 
-    def getColFloat(self):
-        return self.colFloat
+    def getCol(self):
+        res = []
+        i = 0
+        with open(self.dataFile, 'rU') as desc:
+            reader = csv.reader(desc)
+            for row in reader:
+                for cell in row:
+                    if self.header:
+                        res.append(cell)
+                    else:
+                        res.append(i)
+                    i += 1
+                return res
+            
+
+    def run(self, fields):
+        if self.header:
+            head = 1
+        else:
+            head = 0
+        self.data = np.loadtxt(open(self.dataFile,"rb"), usecols=fields, delimiter=",", skiprows=head)
 
     def getData(self):
-        return self.data
+        return np.array(self.data)
